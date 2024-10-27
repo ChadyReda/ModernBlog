3553f5ef23d53406c648f9b2dd144749cf8c6769
chady <chadyxreda@gmail.com>
2024-10-27 23:42:37 +0100

    adding header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,44 +1,30 @@
-"use client"
+"use client";
 
 import { useUser } from "../utils/authContext";
 import Link from "next/link";
 
-
 const Header = () => {
-  const { user, setUser } = useUser()
-
+  const { user, setUser } = useUser();
   const handleLogout = async () => {
-    localStorage.removeItem('token')
-    setUser(null)
-  }
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
   return (
-    <header>
+    <header className="flex items-center justify-between h-[80px] bg-gray-900 w-full md:px-[200px] px-10">
+      <Link href="/" className="text-green-400 text-3xl uppercase font-bold">Hi</Link>
       <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          {!user && (
-            <li>
-              <Link href="/login">Login</Link>
-            </li>
-          )}
-          {!user && (
-            <li>
-              <Link href="/register">Register</Link>
-            </li>
-          )}
-          {user && user.role === "admin" && (
-            <li>
-              <Link href="/admin">Admin</Link>
-            </li>
-          )}
-          {user && (
-            <li>
-              <button onClick={handleLogout}>Logout</button>
-            </li>
-          )}
-        </ul>
+        <Link className="tracking-widest text-neutral-300 mx-3" href="/login">nav1</Link>
+        <Link className="tracking-widest text-neutral-300 mx-3" href="/login">nav2</Link>
+        <Link className="tracking-widest text-neutral-300 mx-3" href="/login">nav3</Link>
+        {!user ? (
+          <>
+            <Link title="dont have an account yet ?" className="tracking-widest text-yellow-400 mx-3" href="/register">register</Link>
+            <Link title="welcome back" className="tracking-widest text-purple-400 mx-3" href="/login">login</Link>
+          </>
+        ): (
+          <button onClick={handleLogout} className="tracking-widest text-red-400 mx-3">logout</button>
+        )}
       </nav>
     </header>
   );
